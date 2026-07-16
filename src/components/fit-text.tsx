df009503d891d697ebc0288c02abbdd9ce10d8eb
EcodiaTate <tate@ecodia.au>
2026-07-16 21:36:03 +1000

    chore(editor): tag data-bound fields as variables (Studio)

--- src/components/fit-text.tsx
+++ src/components/fit-text.tsx
@@ -56,7 +56,7 @@ export function FitText({
   }, [children, max, min])
 
   return (
-    <span
+    <span data-eos-id="src/components/fit-text.tsx#0"
       ref={ref}
       title={title ?? children}
       className={cn('inline-block max-w-full whitespace-nowrap align-bottom', className)}
