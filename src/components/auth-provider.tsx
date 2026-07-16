78d88997ced9fb1935a936b1cb0b6f1a2af5b664
EcodiaTate <tate@ecodia.au>
2026-07-16 21:34:59 +1000

    chore(editor): tag data-bound fields as variables (Studio)

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -9,7 +9,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const auth = useAuthProvider()
 
   return (
-    <AuthContext.Provider value={auth}>
+    <AuthContext.Provider data-eos-id="src/components/auth-provider.tsx#0" value={auth}>
       {children}
     </AuthContext.Provider>
   )
